99bc7585ee1fe0640d4ff1c20874c7d99e6f092e
mikmak29 <mjogamaapple@gmail.com>
2026-01-08 00:15:12 +0800

    Hotfix/removed-unnessary files for further production

--- backend/src/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as userController from "../controllers/user.controller.js";
-
-const route = express.Router();
-
-route.route("/").get(userController.fetchUsers);
-
-export default route;
