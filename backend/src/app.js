18f79593f401ad920d75928ac2913406f3bd2b64
JohnOgama <jogama696@gmail.com>
2026-01-08 23:17:00 +0800

    WIP: backend-integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,21 @@ import cors from "cors";
 import compression from "compression";
 import express from "express";
 
+import userRoutes from "./routes/userRoutes.js";
+
 const app = express();
 
+/**
+ * @param {Object} cors
+ */
+
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    
+}));
 app.use(express.json());
 app.use(compression());
+
+app.use("/api/user", userRoutes);
 
 export default app;
